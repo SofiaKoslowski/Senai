46850e53458284f87f888906529c7ed559d86e04
SofiaKoslowski <sofia_koslowski@estudante.sesisenai.org.br>
2025-05-12 11:29:25 +0000

    update

--- Lista 1/AT. 14.py
+++ Lista 1/AT. 14.py
@@ -1,8 +1,8 @@
 # 14. Faça um algoritmo que receba o preço de um produto, calcule e mostre o novo preço, 
 # sabendo-se que este sofreu um desconto de 10%.
 
-preco = float(input("Qual preço do produto? "))
+preco = float(input("Qual preço do produto? ")) # Insere o valor nas variáveis
 
-desconto = preco * 0.10
+desconto = preco * 0.10 # Faz o calculo
 
-print(f"O preço final é: {desconto}")
+print(f"O preço final é: {desconto}") # Mostra o resultado
